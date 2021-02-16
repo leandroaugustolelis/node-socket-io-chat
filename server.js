720c7b11fe5ee313fae6dfefb423a76418cddf03
Leandro Lelis <leandroaugustolelis@gmail.com>
2021-02-16 11:55:56 -0300

    mongodb connected to store messages

--- server.js
+++ server.js
@@ -1,27 +1,36 @@
 const express = require('express');
-const path = require ('path');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http)
-
+const mongoose = require('mongoose')
 
 app.use(express.json());
 app.use(express.urlencoded());
 
+const dbUrl = 'mongodb+srv://user:user@learning-node.9wpee.mongodb.net/test'
 
-const messages = [
-  {name: 'Leandro', message: 'Hi'},
-  {name: 'Joana', message: 'Hello'},
-]
+const Message = mongoose.model('Message', {
+  name: String,
+  message: String
+})
 
 app.get('/messages',(req,res) => {
-  res.send(messages)
+  Message.find({}, (err,messages) => {
+    res.send(messages)
+  })
+  
 })
 
 app.post('/messages',(req,res) => {
-  messages.push(req.body)
+  const message = new Message(req.body)
+  message.save((err) => {
+    if(err) {
+      res.status(500)
+    }
+    
+  })
   io.emit('message', req.body)
-  res.sendStatus(200);
+  res.status(200);
 })
 
 app.use(express.static(__dirname, () => {
@@ -30,6 +39,10 @@ app.use(express.static(__dirname, () => {
 
 io.on('connection', socket => {
   console.log("a user connected")
+})
+
+mongoose.connect(dbUrl, (err) => {
+  console.log("mongodb connection", err)
 })
 
 const server = http.listen(3000, () => {
